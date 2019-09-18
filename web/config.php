bb5f245ccd48f0c00a0daed7d5dbb155f80ff53a
Laithuhai <45209024+Laithuhai@users.noreply.github.com>
2019-09-18 21:58:49 +0700

    Add files via upload

---
+++ web/config.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'ecomshop');
+define('DB_CHARSET', 'utf8mb4');
+
+// APP ROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL ROOT
+define('URLROOT', 'http://localhost/ecomshop/web/');
+// Site Name
+define('SITENAME', 'Ecommerce Shop');
+// App version
+define('APPVERSION', '1.0.0');
+?>
